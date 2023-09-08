f8bc23bf2e4eb203c830600afc1ad96fea8e9e7a
esso29 <essofawzy20@gmail.com>
2023-09-09 00:44:43 +0300

    tasks

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+    * main - Prints the alphabet in lowercase, except for q and e.
+    *
+    * Return: Always 0.
+*/
+int main(void)
+{
+int num;
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
