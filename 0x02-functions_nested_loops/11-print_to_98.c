89ea45c807bb4187008881e9f39932d86cb06391
esso29 <essofawzy20@gmail.com>
2023-09-13 01:17:44 +0300

    hello

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+    * print_to_98 - print natural numbers ending with 98
+    *@n: is the natural number
+    *return : always 0
+*/
+void print_to_98(int n)
+{
+int x;
+if (n <= 98)
+{
+for (x = n; x <= 98; x++)
+{
+printf("%d", x);
+if (x != 98)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
+else
+{
+for (x = n; x >= 98; x--)
+{
+printf("%d", x);
+if (x != 98)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
+}
